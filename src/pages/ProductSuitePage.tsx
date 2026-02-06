f06d7961da31c1c41c45ed3df5401c10ebb76f33
Geoffrey256 <okumuojgeoffrey@gmail.com>
2026-02-06 16:22:24 +0300

    empty!

--- src/pages/ProductSuitePage.tsx
+++ src/pages/ProductSuitePage.tsx
@@ -1,19 +1,13 @@
-import TopBar from "@/components/TopBar";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import ProductHero from "@/components/ProductHero";
 import ProductSuiteList from "@/components/ProductSuiteList";
 
 const ProductSuitePage = () => {
   return (
     <div className="min-h-screen">
-      <TopBar />
-      <Header />
       <main>
         <ProductHero />
         <ProductSuiteList />
       </main>
-      <Footer />
     </div>
   );
 };
